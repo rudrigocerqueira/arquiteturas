3d4e9076ef7c25ef1da0ca2718aa49a208198ce2
rudrigocerqueira <rodrigocerqueirareis@gmail.com>
2022-10-21 06:27:48 -0300

    Initial Commit

---
+++ arquiteturas/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  arquiteturas
+//
+//  Created by Rodrigo Cerqueira Reis on 21/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
